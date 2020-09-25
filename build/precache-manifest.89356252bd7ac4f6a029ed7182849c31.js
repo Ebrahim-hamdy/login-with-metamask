c04cb420c626edc1262868280f111add194b123f
EbrahimHamdy <ebrahim0hamdy@gmail.com>
2020-09-25 19:16:34 +0300

    add gcp yaml file

---
+++ build/precache-manifest.89356252bd7ac4f6a029ed7182849c31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125e1046216f8c8f42cbc421ff8659b2",
+    "url": "/login-with-metamask/index.html"
+  },
+  {
+    "revision": "38f235098ebe70a4b7fa",
+    "url": "/login-with-metamask/static/css/main.a056d8f2.chunk.css"
+  },
+  {
+    "revision": "bd3f3a7cf287913b309b",
+    "url": "/login-with-metamask/static/js/2.5589b12e.chunk.js"
+  },
+  {
+    "revision": "fddc5c707eccc23815a8617cf795b106",
+    "url": "/login-with-metamask/static/js/2.5589b12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f235098ebe70a4b7fa",
+    "url": "/login-with-metamask/static/js/main.ed2b199d.chunk.js"
+  },
+  {
+    "revision": "26c2c6e0ac01f655084a",
+    "url": "/login-with-metamask/static/js/runtime-main.66d984d5.js"
+  },
+  {
+    "revision": "5206274dacfe6dcbb768b266a8783cec",
+    "url": "/login-with-metamask/static/media/workflow.5206274d.jpg"
+  }
+]);
